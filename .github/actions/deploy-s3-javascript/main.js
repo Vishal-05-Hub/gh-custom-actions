84eebd97b0baa0d63a3be769eb11252274109570
Vishal Chaudhary <vishalchaudhary42929@gmail.com>
2023-03-28 11:53:44 +0530

    Added outputs.

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,8 +11,10 @@ function run() {
     // Upload files
     const s3Uri = `s3://${bucket}`
  // exec.exec(`aws S3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
-    exec.exec("echo ${{s3Uri}}")
+    exec.exec(`echo ${{s3Uri}}`)
 
+    const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteURL);
     core.notice('Hello from my custom javascript action.')
 }
 
